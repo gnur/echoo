7a7dc97af46ad4c87081dd0cfef7b4455d15cdc9
Erwin de Keijzer <erwin@dekeijzer.xyz>
2024-11-13 09:40:02 +0100

    Addded fetch timeout

--- app.vue
+++ app.vue
@@ -1,133 +1,138 @@
 <script setup>
 
 const intervals = [
-  {
-    name: 'disabled',
-    value: 0,
-  },
-  {
-    name: '100ms',
-    value: 100,
-  },
-  {
-    name: '500ms',
-    value: 500,
-  },
-  {
-    name: '1s',
-    value: 1000,
-  },
-  {
-    name: '2s',
-    value: 2000,
-  },
-  {
-    name: '5s',
-    value: 5000,
-  },
-  {
-    name: '10s',
-    value: 10000,
-  },
+    {
+        name: 'disabled',
+        value: 0,
+    },
+    {
+        name: '100ms',
+        value: 100,
+    },
+    {
+        name: '500ms',
+        value: 500,
+    },
+    {
+        name: '1s',
+        value: 1000,
+    },
+    {
+        name: '2s',
+        value: 2000,
+    },
+    {
+        name: '5s',
+        value: 5000,
+    },
+    {
+        name: '10s',
+        value: 10000,
+    },
 ]
 
 let interval = ref(intervals[3].value);
 
 watch(interval, (val, oldval) => {
-  if (oldval == 0 && val > 0) {
-    checkBackend();
-  }
+    if (oldval == 0 && val > 0) {
+        checkBackend();
+    }
 });
 
 let b = {};
 const backends = ref(b);
 
 function updateBackend(backend) {
-  if (backend.name in backends.value) {
-    backends.value[backend.name].requests++;
-    backends.value[backend.name].last = new Date(backend.timestamp)
-  } else {
-    backends.value[backend.name] = {
-      name: backend.name,
-      hostname: backend.hostname,
-      data: backend,
-      first: new Date(backend.timestamp),
-      last: new Date(backend.timestamp),
-      requests: 1,
-    };
-  }
+    if (backend.name in backends.value) {
+        backends.value[backend.name].requests++;
+        backends.value[backend.name].last = new Date(backend.timestamp)
+    } else {
+        backends.value[backend.name] = {
+            name: backend.name,
+            hostname: backend.hostname,
+            data: backend,
+            first: new Date(backend.timestamp),
+            last: new Date(backend.timestamp),
+            requests: 1,
+        };
+    }
 }
 
 async function checkBackend() {
-  if (interval.value == 0) return;
-  //set timeout immediately to ignore the time it takes to fetch
-  setTimeout(checkBackend, interval.value);
-  try {
-    const resp = await fetch("/api");
-    const info = await resp.json();
+    if (interval.value == 0) return;
+    //set timeout immediately to ignore the time it takes to fetch
+    setTimeout(checkBackend, interval.value);
+    try {
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), 5000); // 5 seconds timeout
 
-    updateBackend(info);
-  } catch (e) {
-    updateBackend({
-      name: "error",
-      hostname: "error",
-      timestamp: new Date().getTime(),
-    });
-  }
+        const resp = await fetch("/api", { signal: controller.signal });
+        clearTimeout(timeoutId);
+
+        const info = await resp.json();
+        updateBackend(info);
+    } catch (e) {
+        updateBackend({
+            name: "error",
+            hostname: "error",
+            timestamp: new Date().getTime(),
+        });
+    }
 }
+
 function resetBackends() {
-  backends.value = {};
+    backends.value = {};
 }
 
 onNuxtReady(() => {
-  checkBackend();
+    checkBackend();
 });
 </script>
 
 <style>
 .mycolor {
-  animation: mymove 2s;
+    animation: mymove 2s;
 }
 
 @keyframes mymove {
-  from {
-    background-color: #ee8888;
-  }
+    from {
+        background-color: #ee8888;
+    }
 
-  to {
-    background-color: none;
-  }
+    to {
+        background-color: none;
+    }
 }
 </style>
 
 
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1 class="text-4xl">echoo!</h1>
-          <UButton @click="resetBackends">Reset</UButton>
-          <USelect v-model="interval" :options="intervals" option-attribute="name" size="lg">
-            <template #leading>
-              <UIcon name="i-heroicons-clock" />
+    <UContainer>
+        <UCard class="mt-10">
+            <template #header>
+                <div class="flex justify-between">
+                    <h1 class="text-4xl">echoo!</h1>
+                    <UButton @click="resetBackends">Reset</UButton>
+                    <USelect v-model="interval" :options="intervals" option-attribute="name" size="lg">
+                        <template #leading>
+                            <UIcon name="i-heroicons-clock" />
+                        </template>
+                    </USelect>
+                    <ColorScheme>
+                        <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" size="lg" />
+                    </ColorScheme>
+                </div>
+                <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div class="mycolor m-2 mt-6 w-full p-2 rounded-lg border border-gray-200 shadow-sm truncate"
+                        v-for="(val, index) in backends" :key="val.requests">
+                        <h1 class="h-8 text-2xl font-bold">{{ val.name }}</h1>
+                        Requests: {{ val.requests }}<br>
+                        First: {{ val.first.toLocaleTimeString('nl-NL') }}<br>
+                        Last: {{ val.last.toLocaleTimeString('nl-NL') }}<br>
+                        Host: {{ val.hostname }}<br>
+                    </div>
+                </div>
             </template>
-          </USelect>
-          <ColorScheme>
-            <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" size="lg" />
-          </ColorScheme>
-        </div>
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-          <div class="mycolor m-2 mt-6 w-full p-2 rounded-lg border border-gray-200 shadow-sm truncate"
-            v-for="(val, index) in backends" :key="val.requests">
-            <h1 class="h-8 text-2xl font-bold">{{ val.name }}</h1>
-            Requests: {{ val.requests }}<br>
-            First: {{ val.first.toLocaleTimeString('nl-NL') }}<br>
-            Last: {{ val.last.toLocaleTimeString('nl-NL') }}<br>
-            Host: {{ val.hostname }}<br>
-          </div>
-        </div>
-      </template>
-    </UCard>
-  </UContainer>
+        </UCard>
+    </UContainer>
 </template>
